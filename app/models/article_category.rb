65ab4f870460eefc95f6db355a61f2030497b8d3
Christopher Amanor <kriskit7@gmail.com>
2020-07-09 15:18:32 +0000

    Add associations to articles, categories, votes and article_categories models

--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -1,2 +1,4 @@
 class ArticleCategory < ApplicationRecord
+  belongs_to :category, foreign_key: 'CategoryId', class_name: 'Category'
+  belongs_to :article, foreign_key: 'ArticleId', class_name: 'Article'
 end
