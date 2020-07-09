1fe47c728f091911fde7d9e44adacf261d6a227d
Christopher Amanor <kriskit7@gmail.com>
2020-07-09 15:37:32 +0000

    Add routes for votes, articles, and votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :votes
+  resources :categories
+
+  resources :articles, only: [:index, :create] do
+    # resources :comments, only: [:create]
+    resources :votes, only: [:create, :destroy]
+  end
+
+  root to: "articles#index"
 end
