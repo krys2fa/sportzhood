fb94fc5457784efb6ce691bdb6fbd371950df887
Christopher Amanor <kriskit7@gmail.com>
2020-07-09 15:43:55 +0000

    Fix layout and routes in preparation for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :votes
   resources :categories
 
-  resources :articles, only: [:index, :create] do
+  resources :articles do
     # resources :comments, only: [:create]
     resources :votes, only: [:create, :destroy]
   end
