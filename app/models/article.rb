e3378e4c6e6b3f78832055fd5c47d1b402b9fede
Christopher Amanor <kriskit7@gmail.com>
2020-07-10 08:18:20 +0000

    Add validations to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,7 @@ class Article < ApplicationRecord
   has_many :article_categories, foreign_key: 'ArticleId'
   has_many :categories, through: :article_categories, dependent: :destroy
   has_many :votes, foreign_key: 'ArticleId', class_name: 'Vote', dependent: :destroy
+
+  validates_presence_of :Title, :Text, :AuthorId
+  validates_length_of :Title, :Text, { minimum: 5}
 end
