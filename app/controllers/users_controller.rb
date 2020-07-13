a84002ffda243579d4bea48f48a8cd91074d4e9c
Christopher Amanor <kriskit7@gmail.com>
2020-07-13 22:12:32 +0000

    Add styles to 'Write an Article' page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,11 @@ class UsersController < ApplicationController
   before_action :set_current_user, only: %i[show edit]
   before_action :authenticate_user, except: %i[new create]
 
-  def index
-    # @users = User.all
-  end
-
-  def show; end
+  def index;end
 
   def new
     @user = User.new
   end
-
-  def edit; end
 
   def create
     @user = User.new(user_params)
@@ -40,13 +34,13 @@ class UsersController < ApplicationController
     end
   end
 
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @user.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
 
