18be3be19d888382386ef305a5888081e7d47201
Christopher Amanor <kriskit7@gmail.com>
2020-07-15 12:17:37 +0000

    Add RSpec integration tests for authentication

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -12,22 +12,19 @@ RSpec.feature 'Authentications', type: :feature do
     expect(page).to have_content('WRITE AN ARTICLE')
   end
 
-  # it 'Should sign up' do
-  #   visit new_user_registration_path
-  #   fill_in 'Name', with: 'administrator'
-  #   fill_in 'Email', with: 'adm@admin.com'
-  #   fill_in 'Password', with: 'administrator'
-  #   fill_in 'Password confirmation', with: 'administrator'
-  #   click_button 'commit'
-  #   expect(page).to have_content('Recent posts')
-  # end
+  it 'Should sign up' do
+    visit new_user_path
+    fill_in 'Name', with: 'administrator'
+    fill_in 'Email', with: 'adm@admin.com'
+    click_button 'commit'
+    expect(page).to have_content('Please Sign In')
+  end
 
-  # it 'Should sign out' do
-  #   visit new_user_session_path
-  #   fill_in 'Email', with: 'user@mail.com'
-  #   fill_in 'Password', with: 'asdqwe'
-  #   click_button 'commit'
-  #   expect(page).to have_content('Recent posts')
-  #   click_link 'Sign out'
-  # end
+  it 'Should sign out' do
+    visit sessions_path
+    fill_in 'Name', with: 'User'
+    click_button 'commit'
+    click_link 'Log out'
+    expect(page).to have_content('Please Sign In')
+  end
 end
